507649314e95904402a44c01636ee3ad1f35adb8
Tim Fitzgerald <tim.fitzgerald@unbounce.com>
2018-01-03 11:11:37 -0800

    modified setup links

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 Compiled from elements of:
 
 https://github.com/paulirish/dotfiles 
+
 https://github.com/mathiasbynens/dotfiles/
--- setup.sh
+++ setup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+rm -r ~/.bashrc
+rm -r ~/.bash_profile
+rm -r ~/.aliases
+rm -r ~/.functions
+rm -r ~/.bash_prompt
+
+ln -s ~/dotfiles/.bashrc ~/
 ln -s ~/dotfiles/.bash_profile ~/
 ln -s ~/dotfiles/.aliases ~/
 ln -s ~/dotfiles/.functions ~/
